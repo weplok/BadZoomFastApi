2a9d8ec0714b102effa46dccbbce0e5a5d52b6fe
weplok <jess.makaronin228@gmail.com>
2025-12-08 20:24:13 +0300

    Upgrade chat validation and ban-word list

--- chat_main/validator/processor.py
+++ chat_main/validator/processor.py
@@ -4,29 +4,21 @@ import os
 import re
 
 import ahocorasick
-from fastapi import FastAPI, Request
 from dataclasses import dataclass
+import importlib.resources as res
 
-# Регэксп и банворды
+
+# Регэксп
 ALLOWED_RE = re.compile(r'^[\w\s\-\.\,\!\?\(\)\[\]\{\}\@\#\$\%\^\&\*\:\;\"\'\/\\]+$', re.UNICODE)
-BANWORDS_FILE = "banwords.txt"
+# Регулярка для замены недопустимых символов
+DISALLOWED_RE = re.compile(r'[^\w\s\-\.\,\!\?\(\)\[\]\{\}\@\#\$\%\^\&\*\:\;\"\'\/\\]', re.UNICODE)
+
 
 @dataclass
 class ValidateResult:
     is_valid: bool
     reason: str
     new_message: str = "not required"
-
-
-# Загружаем банворды из файла (если нет — создаём)
-if not os.path.exists(BANWORDS_FILE):
-    with open(BANWORDS_FILE, "w", encoding="utf-8") as f:
-        f.write("дурак\nтупой\nидиот\nлох\nfuck\nshit\nbitch\nasshole\nmoron")
-
-
-def load_banwords():
-    with open(BANWORDS_FILE, "r", encoding="utf-8") as f:
-        return [w.strip().lower() for w in f if w.strip()]
 
 
 # Построение автомата Ахо-Корасика (алгоритм проверки слов)
@@ -38,34 +30,50 @@ def build_automaton(words):
     return A
 
 
-BANWORDS = load_banwords()
+# Берем банворды из data/banwordlist.txt и загружаем в автомат Ахо-Корасика
+BANWORDS = list()
+with res.files("validator.data").joinpath("banwordlist.txt").open("r", encoding="utf-8") as f:
+    for word in f.readlines():
+        BANWORDS.append(word.strip())
 A = build_automaton(BANWORDS)
 
 
 # Асинхронный валидатор
-async def validate_message(text: str) -> tuple[bool, str]:
+async def validate_message(text: str) -> tuple[bool, str, str]:
     await asyncio.sleep(0)  # не блокируем event loop
 
     text = text.strip()
     if not text:
-        return False, "Пустое сообщение"
+        return (False, "Пустое сообщение", " ")
 
     if not ALLOWED_RE.match(text):
-        return False, "Недопустимые символы"
+        sanitized_text = DISALLOWED_RE.sub(" ", text)
+        return (False, "Недопустимые символы", sanitized_text)
 
     lowered = text.lower()
-    for _, found in A.iter(lowered):
-        return False, f"Запрещённое слово: {found}"
+    text = list(text)
+    is_correct = True
+    for i, found in A.iter(lowered):
+        is_correct = False
+        text[(i+1)-len(found):(i+1)] = "*"*len(found)
+    if not is_correct:
+        text = "".join(text)
+        return (False, f"Найдены ban-слова. Цензура: {text}", text)
 
-    return True, "OK"
+    return (True, "OK", "-")
 
 
 # Функция валидации сообщений
 async def process_message(msg_text, sender) -> ValidateResult:
     # ---- Валидация ----
-    is_valid, reason = await validate_message(msg_text)
+    is_valid, reason, new_message = await validate_message(msg_text)
 
     if not is_valid:
-        return ValidateResult(is_valid=is_valid, reason=reason, new_message="Bad message! NEW CORRECT MESSAGE HERE")
+        return ValidateResult(is_valid=is_valid, reason=reason, new_message=new_message)
 
     return ValidateResult(is_valid=is_valid, reason=reason)
+
+
+if __name__ == "__main__":
+    text = "я дурак!"
+    print(validate_message(text))
